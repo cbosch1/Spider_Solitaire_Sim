8fffdb259b5a0928ac1bd4c1cb3c299f7ac5e186
Conner Bosch <cbosch1@mail.usf.edu>
2020-05-06 09:35:30 -0400

    Implimented Deck constructor tests

--- Test_Spider_Sim.py
+++ Test_Spider_Sim.py
@@ -3,34 +3,88 @@ import Spider_Sim
 
 class Test_Stack(unittest.TestCase):
 
-    def Test_Stack_Constructor(self):
+    def test_stack_constructor(self):
 
-        pass
+        self.assertEqual(1, 2)
 
-    def Test_Stack_Pull(self):
+    def test_stack_pull(self):
 
-        pass
+        self.assertEqual(1, 2)
 
-    def Test_Stack_Place(self):
+    def test_stack_place(self):
 
-        pass
+        self.assertEqual(1, 2)
 
 class Test_Deck(unittest.TestCase):
 
-    def Test_Deck_Constructor(self):
+    def test_deck_constructor(self):
 
-        pass
+        full_deck = Spider_Sim.Deck(4)
+        medium_deck = Spider_Sim.Deck(2)
+        low_deck = Spider_Sim.Deck(1)
 
-    def Test_Deck_Deal(self):
+        for card in low_deck.cards:
 
-        pass
+            self.assertEqual(card[1], 0)
+
+        for card in medium_deck.cards:
+
+            try: self.assertEqual(card[1], 0)
+
+            except(AssertionError): 
+                
+                self.assertEqual(card[1], 1)
+
+        for card in full_deck.cards:
+
+            try: self.assertEqual(card[1], 0)
+
+            except(AssertionError):
+
+                try: self.assertEqual(card[1], 1)
+
+                except(AssertionError):
+
+                    try: self.assertEqual(card[1], 2)
+                        
+                    except(AssertionError):
+                        
+                        self.assertEqual(card[1], 3)
+
+    def test_generate_suit(self):
+
+        deck = Spider_Sim.Deck(1)
+        suit = deck.Generate_Suit(0)
+        i = 0
+
+        for card in suit:
+
+            self.assertTupleEqual((i, 0), card)
+
+            i += 1
+
+    def test_deck_deal(self):
+
+        self.assertEqual(1, 2)
 
 class Test_Table(unittest.TestCase):
 
-    def Test_Table_Constructor(self):
+    def test_table_constructor(self):
 
-        pass
+        self.assertEqual(1, 2)
 
-    def Test_Table_Spider_Deal(self):
+    def test_table_spider_deal(self):
 
-        pass
+        self.assertEqual(1, 2)
+
+if __name__ == "__main__":
+
+    suite = unittest.TestSuite()
+
+    suite.addTest(unittest.makeSuite(Test_Stack))
+    suite.addTest(unittest.makeSuite(Test_Deck))
+    suite.addTest(unittest.makeSuite(Test_Table))
+    
+    unittest.TextTestRunner(verbosity=3).run(suite)
+
+
