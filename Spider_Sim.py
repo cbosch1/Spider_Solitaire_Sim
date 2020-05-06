be533d98963f632419bec66a1b77780e55c15ce7
Conner Bosch <cbosch1@mail.usf.edu>
2020-05-06 09:35:44 -0400

    Implimented Deck constructor

--- Spider_Sim.py
+++ Spider_Sim.py
@@ -10,17 +10,15 @@ Structure:
     Deck = [Cards] + Methods
     Table = Deck, [Stacks], + Methods
 
-
 """
 
 class Stack(object):
     """ Contains a list of cards and methods to change that list. """
 
-    def __init__(self, cards):
+    def __init__(self, cards, hidden):
         """ Assumes cards are a tuple of int Card Number and string Suit """
-
-        #TODO: Stack constructor
-        pass
+        self.cards = cards
+        self.hidden = hidden
 
     def Pull(self):
         """ Returns list of cards that are of the same suit and in order """
@@ -41,11 +39,51 @@ class Deck(object):
     """ Contains a list of cards and methods to randomly return cards from said list. """
 
     def __init__(self, num_suit):
-        """ Assumes num_suit is int 
+        """ Assumes num_suit is int.
         Generates cards as a tuple of int Card Number and string Suit"""
         
-        #TODO: Deck constructor
-        pass
+        self.cards = []
+        self.num_suit = num_suit
+
+        if num_suit == 1:
+
+            for i in range(8):
+
+                self.cards.extend(self.Generate_Suit(0))
+
+        if num_suit == 2:
+
+            for i in range(4):
+
+                for suit in range(2):
+
+                    self.cards.extend(self.Generate_Suit(suit))
+
+        if num_suit == 4:
+
+            for i in range(2):
+
+                for suit in range(4):
+
+                    self.cards.extend(self.Generate_Suit(suit))
+
+        if num_suit != 1 and num_suit != 2 and num_suit != 4:
+
+            raise(ValueError("Incorrect Suit Value"))
+
+    def Generate_Suit(self, suit):
+        """ Assists constructor by creating a 13 card list
+        with the suit determined by input.
+        Returns a list of tuples """
+
+        cards = []
+
+        for card_num in range(13):
+
+            cards.append((card_num, suit))
+
+        return cards
+
 
     def Deal(self, num_cards):
         """ Assumes num_cards is int
@@ -57,12 +95,13 @@ class Deck(object):
 class Table(object):
     """ Contains a list of stacks and a Deck, performs operations to play the game. """
 
-    def __init__(self, stacks, deck, num_suit):
+    def __init__(self, num_suit):
         """ Assumes num_suit is int
         Generates Deck and then list of Stacks off num_suit"""
 
-        #TODO: Table constructor
-        pass
+        self.num_suit = num_suit
+        self.deck = Deck(num_suit)
+        self.stacks = []
 
     def Spider_Deal(self):
         """ Calls Deck.Deal for each stack on the table
