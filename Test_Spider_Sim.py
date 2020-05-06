b94787667b15acc70c8e96a52dae4c1d750f139e
Conner Bosch <cbosch1@mail.usf.edu>
2020-05-06 08:19:11 -0400

    Added Unittest functionality

---
+++ Test_Spider_Sim.py
@@ -0,0 +1,36 @@
+import unittest
+import Spider_Sim
+
+class Test_Stack(unittest.TestCase):
+
+    def Test_Stack_Constructor(self):
+
+        pass
+
+    def Test_Stack_Pull(self):
+
+        pass
+
+    def Test_Stack_Place(self):
+
+        pass
+
+class Test_Deck(unittest.TestCase):
+
+    def Test_Deck_Constructor(self):
+
+        pass
+
+    def Test_Deck_Deal(self):
+
+        pass
+
+class Test_Table(unittest.TestCase):
+
+    def Test_Table_Constructor(self):
+
+        pass
+
+    def Test_Table_Spider_Deal(self):
+
+        pass
